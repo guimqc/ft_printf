81c67851b3b0e9c48c7bf47993087fe1905e8351
vial <guillaumevial15@gmail.com>
2022-04-03 17:58:38 -0400

    some warning cases

--- main.c
+++ main.c
@@ -3,5 +3,9 @@
 
 int	main()
 {
-	ft_printf("%i hello my name is %s and i am %i yrs old %i");
+	// warning: more '%' conversions than data arguments
+	// printf("%i hello my name is %s and i am %i yrs old %i", age, "hello");
+	
+	// warning: data argument not used by format string
+	// printf("hello\n", age);
 }
