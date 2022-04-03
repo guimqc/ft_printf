145948d8f094fcfd090729acb5c6fba8e9cd7df6
vial <guillaumevial15@gmail.com>
2022-04-03 12:27:25 -0400

    make test && ./test "str passed to ft_printf"

--- ft_printf.c
+++ ft_printf.c
@@ -1,7 +1,21 @@
 #include "printf.h"
 #include <stdio.h>
 
-int main()
+int ft_printf(const char *s, ...)
 {
-    printf("test: %s\n", ft_itoa(1024));
+	write(1, s, ft_strlen((char *)s));
+	write(1, "\n", 1);
+
+	return (42);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		ft_printf((const char *)argv[1]);
+		printf("printf = %s\n", argv[1]);
+	}
+	else
+		printf("arg error");
 }
