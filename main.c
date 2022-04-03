bf34f28cb59f8d750d66c2c277f11a9c6d689d09
vial <guillaumevial15@gmail.com>
2022-04-03 13:17:29 -0400

    make test && ./test "str passed to ft_printf"

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int	main(int argc, char **argv)
 	if (argc == 2)
 	{
 		ft_printf((const char *)argv[1]);
-		printf("%s (real)\n", argv[1]);
+		printf("(real) %s", argv[1]);
 	}
 	else
 		printf("arg error");
