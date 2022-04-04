e3f3fe30cc090a6f4343e0f790fb49fcc124b37b
Guillaume Vial <gvial@c1r4p7.42quebec.com>
2022-04-04 11:56:27 -0400

    count_arg

--- count_arg.c
+++ count_arg.c
@@ -7,8 +7,10 @@ int count_arg(char *s)
 
     i = 0;
     count = 0;
-    while (i < ft_strlen(s))
-        if (s[i++] == '%')
+    while (i < ft_strlen(s))                     cspdiuxX%
+        if ((s[i++] == '%') && (s[i + 1] == 'c' || s[i + 1] == 's' || s[i + 1] == 'p' 
+            || s[i + 1] == 'd' || s[i + 1] == 'i' || s[i + 1] == 'u' || s[i + 1] == 'x' 
+            || s[i + 1] == 'X' || s[i + 1] == '%'))
             count++;
     return (count);
 }
