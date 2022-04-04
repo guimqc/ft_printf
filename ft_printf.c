b59f5d3afcc9a7c93d81c56befce7df47db5e3c3
Guillaume Vial <gvial@c1r4p7.42quebec.com>
2022-04-04 13:53:26 -0400

    count_arg

--- ft_printf.c
+++ ft_printf.c
@@ -1,12 +1,13 @@
 #include "printf.h"
 #include <stdio.h>
+#include <stdarg.h>
 
 int	ft_printf(const char *s, ...)
 {
 	int arg_count;
 
 	arg_count = count_arg((char *)s);
-	printf("arg count: %i\n", arg_count);
+	// printf("arg count: %i\n", arg_count);
 
 	write(1, s, ft_strlen((char *)s));
 
