310b850404558ee679896544068a4ac4335dd584
Guillaume Vial <gvial@c1r4p9.42quebec.com>
2022-04-07 12:45:47 -0400

    itoa

--- format_and_print.c
+++ format_and_print.c
@@ -20,6 +20,6 @@ int	format_and_print(va_list arg_list, char format)
 	if (format == 'p')
 		len += print_adress(va_arg(arg_list, long));
 	if (format == 'u')
-		return ;
+		return (0);
 	return (len);
 }
