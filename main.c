e2887ce4cf9e4b6f01a31a18f041deb72ee17205
vial <guillaumevial15@gmail.com>
2022-04-07 00:05:34 -0400

    main

--- main.c
+++ main.c
@@ -2,11 +2,6 @@
 
 int	main()
 {
-	// warning: more '%' conversions than data arguments
-	// printf("%i hello my name is %s and i am %i yrs old %i", age, "hello");
-	
-	// warning: data argument not used by format string
-	// printf("hello\n", age);
 	char c = 'a';
 	char *s = "test";
 	int i = 456;
