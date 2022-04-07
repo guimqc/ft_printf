5da6510bc3603f22e9cbe0fa56896c99b106f219
Guillaume Vial <gvial@c1r4p9.42quebec.com>
2022-04-07 12:03:51 -0400

    main

--- main.c
+++ main.c
@@ -21,25 +21,25 @@ int	main()
 	int str_len = printf("(char) %c\n", 'v');
 	int my_str_len = ft_printf("(char) %c\n", 'v');
 	printf("real return = %i\n", str_len);
-	printf("my return = %i\n\n", my_str_len);
+	ft_printf("my return = %i\n\n", my_str_len);
 
 	int str_len1 = printf("(hexa) %x\n", 12);
 	int my_str_len1 = ft_printf("(hexa) %x\n", 12);
 	printf("real return = %i\n", str_len1);
-	printf("my return = %i\n\n", my_str_len1);
+	ft_printf("my return = %i\n\n", my_str_len1);
 
 	int str_len2 = printf("(pointer) %p\n", p);
 	int my_str_len2 = ft_printf("(pointer) %p\n", p);
 	printf("real return = %i\n", str_len2);
-	printf("my return = %i\n\n", my_str_len2);
+	ft_printf("my return = %i\n\n", my_str_len2);
 
 	int str_len3 = printf("(string) %s\n", s);
 	int my_str_len3 = ft_printf("(string) %s\n", s);
 	printf("real return = %i\n", str_len3);
-	printf("my return = %i\n\n", my_str_len3);
+	ft_printf("my return = %i\n\n", my_str_len3);
 
 	int str_len4 = printf("(number) %i\n", i);
 	int my_str_len4 = ft_printf("(number) %i\n", i);
 	printf("real return = %i\n", str_len4);
-	printf("my return = %i\n\n", my_str_len4);
+	ft_printf("my return = %i\n\n", my_str_len4);
 }
